e204ccc9967eadbd359faa2c2a02c5b167f14486
Kartik K. Agaram <vc@akkartik.com>
2009-01-19 20:35:20 -0800

    6

--- lib/update_feeds.rb
+++ lib/update_feeds.rb
@@ -4,8 +4,10 @@ def updateAllFeeds
   File.read("#{RAILS_ROOT}/feeds").each do |url|
     parsedFeed = rfp(url)
     parsedFeed.entries.each do |entry|
-      Post.create_or_update :url => entry.link, :title => entry.title, :feedurl => p.channel.link, :feedtitle => p.channel.title
+      fields = {:url => entry.link, :title => entry.title, :feedurl => parsedFeed.channel.link, :feedtitle => parsedFeed.channel.title}
+      p = Post.find_or_create_by_url fields
+      p.update_attributes fields
+      p.save
     end
-    break
   end
 end
