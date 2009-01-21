0658296adaee5ecbb1885ec28eb3acb87e07726e
Kartik K. Agaram <vc@akkartik.com>
2009-01-20 18:31:32 -0800

    11

--- lib/update_feeds.rb
+++ lib/update_feeds.rb
@@ -1,9 +1,20 @@
 require 'rfeedparser'
 
+def getFeedWithRetries(url, num=10)
+  done = false
+  until done
+    begin
+      return rfp(url)
+    rescue Timeout::Error
+      num -= 1
+    end
+  end
+end
+
 def updateAllFeeds
   File.read("#{RAILS_ROOT}/config/feeds").each do |url|
     puts url
-    parsedFeed = rfp(url)
+    parsedFeed = getFeedWithRetries(url)
     parsedFeed.entries.each do |entry|
       fields = {:url => entry.link, :title => entry.title, :feedurl => parsedFeed.channel.link, :feedtitle => parsedFeed.channel.title, :contents => entry.description}
       p = Post.find_or_create_by_url fields
