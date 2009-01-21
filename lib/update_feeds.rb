8425b77f2eec9eb9c256c15e8f914266a8fa1e95
Kartik K. Agaram <vc@akkartik.com>
2009-01-20 18:35:17 -0800

    12

--- lib/update_feeds.rb
+++ lib/update_feeds.rb
@@ -6,6 +6,7 @@ def getFeedWithRetries(url, num=10)
     begin
       return rfp(url)
     rescue Timeout::Error
+      puts "timeout #{num}"
       num -= 1
     end
   end
@@ -15,6 +16,7 @@ def updateAllFeeds
   File.read("#{RAILS_ROOT}/config/feeds").each do |url|
     puts url
     parsedFeed = getFeedWithRetries(url)
+    next if parsedFeed.nil?
     parsedFeed.entries.each do |entry|
       fields = {:url => entry.link, :title => entry.title, :feedurl => parsedFeed.channel.link, :feedtitle => parsedFeed.channel.title, :contents => entry.description}
       p = Post.find_or_create_by_url fields
