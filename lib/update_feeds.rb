63fb6ea6283c4b456fdc5b07ac15a1032b5e8e18
Kartik K. Agaram <vc@akkartik.com>
2009-01-21 08:30:45 -0800

    13

--- lib/update_feeds.rb
+++ lib/update_feeds.rb
@@ -1,10 +1,9 @@
 require 'rfeedparser'
 
-def getFeedWithRetries(url, num=10)
-  done = false
-  until done
+def retryTimeout(num=10)
+  until num < 0
     begin
-      return rfp(url)
+      yield
     rescue Timeout::Error
       puts "timeout #{num}"
       num -= 1
@@ -12,11 +11,22 @@ def getFeedWithRetries(url, num=10)
   end
 end
 
+def getFeedWithRetries(url)
+  retryTimeout do
+    rfp(url)
+  end
+end
+
 def updateAllFeeds
   File.read("#{RAILS_ROOT}/config/feeds").each do |url|
     puts url
-    parsedFeed = getFeedWithRetries(url)
-    next if parsedFeed.nil?
+    begin
+      parsedFeed = rfp(url)
+    rescue Timeout::Error
+      puts "timeout. skipping"
+      next
+    end
+
     parsedFeed.entries.each do |entry|
       fields = {:url => entry.link, :title => entry.title, :feedurl => parsedFeed.channel.link, :feedtitle => parsedFeed.channel.title, :contents => entry.description}
       p = Post.find_or_create_by_url fields
