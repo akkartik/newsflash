d4a56b3ceda82d7081ae2e61cc0a9bab812354ae
Kartik K. Agaram <vc@akkartik.com>
2009-01-21 19:12:18 -0800

    16

--- lib/update_feeds.rb
+++ lib/update_feeds.rb
@@ -11,10 +11,14 @@ def updateAllFeeds
     end
 
     parsedFeed.entries.each do |entry|
-      fields = {:url => entry.link, :title => entry.title, :feedurl => parsedFeed.channel.link, :feedtitle => parsedFeed.channel.title, :contents => entry.description}
-      p = Post.find_or_create_by_url fields
-      p.update_attributes fields
-      p.save
+      puts "=="
+      puts "title: #{!entry.title.nil?} #{!entry.link.nil?} #{!entry.description.nil?}"
+      p entry
+
+#?       fields = {:url => entry.link, :title => entry.title, :feedurl => parsedFeed.channel.link, :feedtitle => parsedFeed.channel.title, :contents => entry.description}
+#?       p = Post.find_or_create_by_url fields
+#?       p.update_attributes fields
+#?       p.save
     end
   end
 end
