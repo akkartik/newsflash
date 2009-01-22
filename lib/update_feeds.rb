f90f3a17091d1d93287b28c1a78432a9ea3c0654
Kartik K. Agaram <vc@akkartik.com>
2009-01-21 20:05:24 -0800

    18

--- lib/update_feeds.rb
+++ lib/update_feeds.rb
@@ -1,21 +1,29 @@
 require 'rfeedparser'
 
+def atomPermalink(entry)
+  entry.links.select{|elem| ['alternate', "'alternate'"].include? elem['rel']}[0].href
+end
+
 def updateAllFeeds
-  File.read(FEED_FILE).each do |url|
-    puts url
+  File.read(FEED_FILE+".new").each do |feedUrl|
+    puts feedUrl
     begin
-      parsedFeed = rfp(url)
+      parsedFeed = rfp(feedUrl)
     rescue Timeout::Error
       puts "timeout. skipping"
       next
     end
 
     parsedFeed.entries.each do |entry|
-      puts "=="
-      puts "title: #{!entry.title.nil?} #{!entry.link.nil?} #{!entry.description.nil?}"
-      p entry
+      description = entry.description || entry.content[0].value
+      url = entry.link || atomPermalink(entry)
+#?       url = entry.link || entry.links[-1].href
+      p url
+#?       puts "title: #{!entry.title.nil?} #{!url.nil?} #{!description.nil?}"
+#?       p entry
+      break
 
-#?       fields = {:url => entry.link, :title => entry.title, :feedurl => parsedFeed.channel.link, :feedtitle => parsedFeed.channel.title, :contents => entry.description}
+#?       fields = {:url => entry.link, :title => entry.title, :feedurl => feedUrl, :feedtitle => parsedFeed.channel.title, :contents => entry.description}
 #?       p = Post.find_or_create_by_url fields
 #?       p.update_attributes fields
 #?       p.save
