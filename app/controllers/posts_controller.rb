765585fcf3fd502d4ccb9d4831ea83ef140155c3
Kartik K. Agaram <vc@akkartik.com>
2009-01-23 07:29:31 -0800

    34

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,20 @@
 class PostsController < ApplicationController
   def show
-    return @post = Post.most_recent_from_next_feed(params[:url]) if params[:next]
+    p "show"
     @post = Post.find_by_url(params[:url])
   end
 
   def index
     @post = Post.most_recent_post(Post.first_feed)
+  end
+
+  def update
+    p "update"
+    curr_post = Post.find_by_url(params[:url])
+    curr_post.doneReading = true
+    curr_post.save
+
+    @post = Post.most_recent_from_next_feed(params[:url])
+    p "@post is now #{@post.url}"
   end
 end
