91ff2e222d59d249fbf62b5cca9b5b29c304c1a5
Kartik K. Agaram <vc@akkartik.com>
2009-01-23 21:13:36 -0800

    43

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def fontSize(s)
+    return '100' if s.nil?
+    l = s.length
+    p l
+    return '100' if l > 2000
+    return '200' if l < 500
+    100+(2000-l)/15
+  end
 end
